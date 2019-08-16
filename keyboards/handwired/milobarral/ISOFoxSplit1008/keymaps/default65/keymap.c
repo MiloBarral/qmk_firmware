df3674070ff7e107c5c69400362605e5cdc2edee
MiloBarral <milobb@gmail.com>
2019-08-16 15:32:19 +0200

    Update keymap.c

--- keyboards/handwired/milobarral/ISOFoxSplit1008/keymaps/default65/keymap.c
+++ keyboards/handwired/milobarral/ISOFoxSplit1008/keymaps/default65/keymap.c
@@ -14,12 +14,26 @@ enum custom_keycodes {
   QWERTY = SAFE_RANGE,
 };
 
+/* ISO EXAMPLE:
+
+[ BASE ] = KEYMAP(
+  KC_ESC,         KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS, KC_EQL,      KC_BSPC, DEAD,\
+  KC_TAB,         KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC, KC_RBRC,     DEAD,    \
+  OSL(CAPS),      KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT, KC_NUHS,        KC_ENT,  \
+  KC_LSHIFT,      KC_NUBS,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH, KC_RSFT,     DEAD, \
+  KC_LCTL,        KC_LGUI,KC_LALT,                LT(SPACE, KC_SPC),                     OSL(ALT),OSL(FN1),OSL(FN2),    KC_RCTL)
+
+*/
+/*
+Most of the time this key will output KC_ESC when pressed. However, when Shift or GUI are held down it will output KC_GRV instead.
+*/
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT_65(
     KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_DEL,           KC_BSPC, KC_HOME, \
-    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,             KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_LBRC, KC_RBRC, KC_END,  \
+    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,             KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_RBRC, KC_END,  \
     KC_BSPC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,             KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_SCLN, KC_QUOT,          KC_PGUP, \
-    KC_LSFT, KC_LABK, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   KC_PGDN, \
+    KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   KC_PGDN, \
     KC_LCTL, KC_LALT, KC_LGUI, MO(_L1), KC_SPC,                    KC_SPC,           MO(_L1), KC_RALT, KC_RCTL, KC_RGUI, KC_LEFT, KC_DOWN, KC_RGHT
   ),
 
